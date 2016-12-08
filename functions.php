c0e13841bc739baa5797af14ab370dfb0cb785fd
Jaromir Obr <jaromir.obr@gmail.com>
2016-12-08 20:51:18 +0100

    Added functions example

--- functions.php
+++ functions.php
@@ -20,5 +20,50 @@ hlavicka();
 echo "<BODY>";
 printText("Tělo stránky", 1);
 printText("Hello page", 5);
+printText(dvaplusdva(), 3);
 echo "</BODY></HTML>";
+?>
+
+<?php
+function dvaplusdva()
+{
+  echo "Printing" . "<BR>";
+  return 2+2;
+  echo "No printing";
+}
+?>
+
+<?php
+function JeNedele($den)
+{
+  return (date("w", strtotime($den))==0);
+}
+$day = "2016-12-10";
+echo $day . (JeNedele($day) ? " je neděle" : " není neděle");
+echo "<BR>";
+?>
+
+<?php
+function Hlaska (&$hlaska, &$names)
+{
+  global $VracetUpovidaneHlasky; // otherwise function doesn't know about the variable
+  static $nochange = "Jedna"; // remember value when function exits
+  echo $nochange, "<BR>";
+  if ($VracetUpovidaneHlasky)
+    echo "Já jsem velmi upovídaná hláška.";
+  else
+    echo "Hláška";
+  echo "<BR>";  
+  $hlaska = "Nekoukej";
+  $nochange = "Dva";
+  array_push($names, "Franta");
+}
+$hl = "Jo";
+$VracetUpovidaneHlasky = TRUE;
+$names = ["Mira", "Alfons"];
+Hlaska($hl, $names);
+Hlaska($hl, $names);
+
+echo $hl, "<BR>";
+print_r($names);
 ?>
