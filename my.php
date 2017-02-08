6694eff066fd19ab6eef9dff8f7d5ba70a543b20
Jaromir Obr <jaromir.obr@gmail.com>
2017-02-08 19:48:29 +0100

    Long php string

--- my.php
+++ my.php
@@ -7,4 +7,10 @@
     <td>Collection ID</td>
     <td>Collection Name</td>
 <tr>
+</table>
+<?php
+$note = "<i>Click on the link above to see just Syllabi assigned to your advisees.
+<br />Choose the SYLLABI menu if you want to see all syllabi.</i>";
+echo $note;
+?>
 <i>italika</i>
