861b660cc3eca8d9eebd5b795c6488683c331cf3
Jaromir Obr <jaromir.obr@gmail.com>
2017-03-07 19:48:26 +0100

    Added examples for work with array

--- array.php
+++ array.php
@@ -44,5 +44,55 @@ print_r($fronta);
 $frontab = ["first_name" => "Mira", "last_name" => "Obr"];
 print_r($frontab);
 var_dump($frontab);
-print_r(array_keys($frontab));
+echo '<pre>'; print_r(array_keys($frontab)); echo '</pre>';
+echo '<pre>'; print_r($frontab); echo '</pre>';
+
+$os=Array("íránec", "Linux", "Čeština", "Windows", "solaris", "FreeBSD");
+echo '<pre>'; print_r($os); echo '</pre>';
+$os_to_sort = $os;
+setlocale(LC_COLLATE, "cs_CZ.UTF-8");
+sort($os_to_sort, SORT_LOCALE_STRING);
+echo '<pre>Sorted:<br>'; print_r($os_to_sort); echo '</pre>';
+rsort($os_to_sort, SORT_LOCALE_STRING);
+echo '<pre>RSorted:<br>'; print_r($os_to_sort); echo '</pre>';
+
+foreach ($os as $my_os)
+   echo "Systém: $my_os<br>\n";
+
+for ($i = 0; $i < count($os); ++$i)
+    echo "$i: $os[$i]<br>";
+
+echo '<pre>'; print_r(explode(" ", "Ret e zec")); echo '</pre>';
+echo '<pre>'; print_r(implode("", $os)); echo '</pre>';
+
+echo '<pre>'; print_r(str_split("Ret e zec")); echo '</pre>';
+
+define ("CENZUROVANO", "[ !!! Censored !!!]");
+
+function cenzuruj($text)
+{
+  $SpatnaSlova = Array("Windows", "Microsoft", "databáze");
+  $RozdelText = explode(" ", $text);
+  foreach ($RozdelText as $TestovaneSlovo) {
+    if (in_array($TestovaneSlovo, $SpatnaSlova))
+      $TestovaneSlovo=CENZUROVANO;
+   $PoCenzure=$PoCenzure." ".$TestovaneSlovo;
+  }
+  return $PoCenzure;
+}
+
+// alternative solution
+function cenzuruj2($text)
+{
+  $SpatnaSlova = Array("Windows", "Microsoft", "databáze");
+  foreach ($SpatnaSlova as $SpatneSlovo)
+    $text = str_replace($SpatneSlovo, CENZUROVANO, $text);
+  return $text;
+}
+echo cenzuruj("Nejlepší softwarová firma na světě je Microsoft."), "<br>";
+echo cenzuruj(" Její operační systém Windows je ukázkou výkonu, stability a bezpečnosti."), "<br>";
+echo cenzuruj("Linuxová databáze is the best, stejně tak Windows databáze!"), "<br>";
+echo cenzuruj2("Nejlepší softwarová firma na světě je Microsoft."), "<br>";
+echo cenzuruj2(" Její operační systém Windows je ukázkou výkonu, stability a bezpečnosti."), "<br>";
+echo cenzuruj2("Linuxová databáze is the best, stejně tak Windows databáze!"), "<br>";
 ?>
