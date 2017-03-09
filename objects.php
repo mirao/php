f6f2fdeabda84a0bada99344a4033fe2dd53fa54
Jaromir Obr <jaromir.obr@gmail.com>
2017-03-09 12:43:37 +0100

    Added an example with objects

---
+++ objects.php
@@ -0,0 +1,34 @@
+<?php
+class Car
+{
+    var $type, $color, $desc;
+    static $descGlobal = "It's a car";
+
+    static function printDesc()
+    {
+        echo "Global desc: " . self::$descGlobal, PHP_EOL;
+    }
+
+    function car($type = "Trabant", $color = "White", $desc = "Classic")
+    {
+        $this->type = $type;
+        $this->color = $color;
+        $this->desc = $desc;
+    }
+
+    function print()
+    {
+        echo "Type: " . $this->type, PHP_EOL;
+        echo "Color: " . $this->color, PHP_EOL;
+        echo "Desc: " . $this->desc, PHP_EOL;
+        self::printDesc();
+    }
+}
+
+$car = new Car;
+$car->type = "Ferrari";
+$car->color = "Red";
+$car->print();
+$car2 = new Car("Audi", "Blue", "Very modern");
+$car2->print();
+Car::printDesc(); // print desc globally
