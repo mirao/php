e49fcaf470cfade5bf8fc3b390e6668f79aa47c2
Jaromir Obr <jaromir.obr@gmail.com>
2017-07-29 23:50:50 +0200

    DB extended by PDO

--- mysql/Db.php
+++ mysql/Db.php
@@ -2,19 +2,16 @@
 require "config.php";
 
 /** Connect and work with DB */
-class Db
+class Db extends PDO
 {
-    public $dbh;
-
     function __construct()
     {
         // Connect to DB
-        $this->dbh = new PDO('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DBNAME, SQL_USERNAME, SQL_PASSWORD);
-        $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        parent::__construct('mysql:host=' . SQL_HOST . ';dbname=' . SQL_DBNAME, SQL_USERNAME, SQL_PASSWORD);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    function __destruct()
-    {
-        $this->dbh = null;
+    public function last_row_count() {
+        return $this->query("SELECT FOUND_ROWS()")->fetchColumn();
     }
 }
