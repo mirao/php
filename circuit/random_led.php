1fdb3e44b12a3f8473bcaa520f2385576d7e09ba
Jaromir Obr <jaromir.obr@gmail.com>
2018-06-20 21:22:27 +0200

    Added optional second parameter - start number

--- circuit/random_led.php
+++ circuit/random_led.php
@@ -1,57 +1,60 @@
 <?php
 /** 
- * Record number created by LED(s)
+ * Record numbers created by random LED states
  * See:
  * https://maly.gitbooks.io/hradla-volty-jednocipy/19_panna,_nebo_orel/
  * https://maly.gitbooks.io/hradla-volty-jednocipy/20_citace/203_hraci_kostka.html
+ * 
  * Sample usage
  * ------------
- * php random_led.php # Numbers 0, 1
- * php random_led.php 6 # Numbers 0, 1, 2, 3, 4, 5
+ * php random_led.php # States 0, 1
+ * php random_led.php 6 # States 0, 1, 2, 3, 4, 5
+ * php random_led.php 6 1 # Cube mode, states 1, 2, 3, 4, 5, 6
  */
 
 /**
  * Print header with number columns
- * @param int $numCnt Count of numbers
+ * @param int $stateCnt Count of LED states
+ * @param int $startNum Start number
  */ 
-function printHeader(int $numCnt)
+function printHeader(int $stateCnt, int $startNum)
 {
-    echo "Press number 0 - " . ($numCnt - 1) . " after every LED result. Abort by Ctrl+C\n";
-    for ($i = 0; $i < $numCnt; $i++) {
+    echo "Press number $startNum - " . ($startNum + $stateCnt - 1) . " after every LED result. Abort by Ctrl+C" . PHP_EOL;
+    for ($i = $startNum; $i < $stateCnt + $startNum; $i++) {
         echo "$i\t";
     }
-    echo "\n";
+    echo PHP_EOL;
 }
 
 /**
  * Print current stats after each try
  * @param array $tries Tries
  * @param int $totalTriesCnt Total tries count
- * @param int $numCnt Count of numbers
- */ 
-function printStats(array $tries, $totalTriesCnt, $numCnt)
+ */
+function printStats(array $tries, $totalTriesCnt)
 {
-    for ($i = 0; $i < $numCnt; $i++) {
-        echo round($tries[$i] / $totalTriesCnt * 100) . "%\t";
+    foreach ($tries as $tryCnt) {
+        echo round($tryCnt / $totalTriesCnt * 100) . "%\t";
     }
-    echo "\n";
+    echo PHP_EOL;
 }
 
 system('stty cbreak -echo');
 $stdin = fopen('php://stdin', 'r');
 $totalTriesCnt = 0;
 $tries = [];
-$numCnt = isset($argv[1]) ? $argv[1] : 2;
-for ($i = 0; $i < $numCnt; $i++) {
+$stateCnt = $argv[1] ?? 2; // Default value is "2 states" - on/off
+$startNum = $argv[2] ?? 0; // Default start number is 0
+for ($i = $startNum; $i < $startNum + $stateCnt; $i++) {
     $tries[$i] = 0;
 }
 
-printHeader($numCnt);
+printHeader($stateCnt, $startNum);
 while (1) {
     $response = fgetc($stdin);
-    if ($response >= "0" && $response < $numCnt) {
+    if ($response >= "$startNum" && $response < (string) ($startNum + $stateCnt)) {
         $totalTriesCnt++;
         $tries[$response]++;
-        printStats($tries, $totalTriesCnt, $numCnt);
+        printStats($tries, $totalTriesCnt, $stateCnt);
     }
 }
